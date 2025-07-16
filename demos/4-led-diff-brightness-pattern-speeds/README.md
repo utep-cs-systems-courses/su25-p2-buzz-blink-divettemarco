0d1ea1bb669e56cd486e3d4dff4dfb67a492b5cf
divettemarco <divettemarco@outlook.com>
2025-07-16 00:39:19 -0600

    blink green and red leds dim to bright faster

---
+++ demos/4-led-diff-brightness-pattern-speeds/README.md
@@ -0,0 +1,12 @@
+## Description
+
+This program implements mutliple state machines where every half second, the
+red and the green LED's limit is adjusted (0 =< n =< 8). green and red LEDs
+go from dim to bright.
+
+* one to blink the led with some duty cycle
+
+* one to modify the blink duty cycle once each half second
+
+* one to cause an event every half second
+
---
+++ demos/4-led-diff-brightness-pattern-speeds/main.c
@@ -0,0 +1,64 @@
+//Alternate LEDs from Off, Green, and Red
+#include <msp430.h>
+#include "libTimer.h"
+#include "led.h"
+
+int main(void) {
+  P1DIR |= LEDS;
+  /* green off, red on  */
+  P1OUT &= ~LED_GREEN;
+  P1OUT &= ~LED_RED;
+
+  configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
+  enableWDTInterrupts();	/* enable periodic interrupt */
+  
+  or_sr(0x18);			/* CPU off, GIE on */
+}
+
+// global state vars that control blinking
+int greenLimit = 5;  // duty cycle = 1/blinkLimit
+int greenCount = 0;  // cycles 0...blinkLimit-1
+
+int redLimit = 5;
+int redCount = 0;
+
+int secondCount = 0; // state var representing repeating time 0…1s
+
+void
+__interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
+{
+  // handle blinking 
+  greenCount ++;
+  redCount ++;
+
+  /* turn on green LED every time the limit is hit */
+  if (greenCount >= greenLimit) { // on for 1 interrupt period
+    greenCount = 0;
+    P1OUT |= LED_GREEN;
+  } else		          // off for blinkLimit - 1 interrupt periods
+    P1OUT &= ~LED_GREEN;
+
+  /* turn on red LED every time the limit is hit */
+  if (redCount >= redLimit) {
+    redCount = 0;
+    P1OUT |= LED_RED;
+  }else
+    P1OUT &= ~LED_RED;
+
+  // measure a half second
+  secondCount ++;
+  if (secondCount >= 125) {  // once each half second
+    secondCount = 0;
+    
+    /* adjust green LED duty cycle*/
+    greenLimit --;	     // increase duty cycle
+    if (greenLimit <= 0)    // but don't let duty cycle go over 1/7.
+      greenLimit = 8;
+
+    /* adjust red LED duty cycle*/
+    redLimit --;
+    if (redLimit <= 0) 
+      redLimit = 8;
+  }
+} 
+

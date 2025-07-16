567c567af73deb1a576eef9e4308c0ae00c3376b
divettemarco <divettemarco@outlook.com>
2025-07-16 00:07:20 -0600

    blink green LED from dim to bright states

---
+++ demos/4-led-blink-dim-to-bright/main.c
@@ -0,0 +1,45 @@
+//Alternate LEDs from Off, Green, and Red
+#include <msp430.h>
+#include "libTimer.h"
+#include "led.h"
+
+int main(void) {
+  P1DIR |= LEDS;
+  /* green off, red on  */
+  P1OUT &= ~LED_GREEN;
+  P1OUT |= LED_RED;
+
+  configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
+  enableWDTInterrupts();	/* enable periodic interrupt */
+  
+  or_sr(0x18);			/* CPU off, GIE on */
+}
+
+// global state vars that control blinking
+int blinkLimit = 5;  // duty cycle = 1/blinkLimit
+int blinkCount = 0;  // cycles 0...blinkLimit-1
+int secondCount = 0; // state var representing repeating time 0…1s
+
+void
+__interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
+{
+  // handle blinking 
+  blinkCount ++;
+  if (blinkCount >= blinkLimit) { // on for 1 interrupt period
+    blinkCount = 0;
+    P1OUT |= LED_GREEN;
+  } else		          // off for blinkLimit - 1 interrupt periods
+    P1OUT &= ~LED_GREEN;
+
+  // measure a second
+  secondCount ++;
+  if (secondCount >= 250) {  // once each second
+    secondCount = 0;
+    P1OUT ^= LED_RED; // toggle red LED
+    blinkLimit --;	     // increase duty cycle
+    //    if (blinkLimit >= 8)     // but don't let duty cycle go over 1/7.
+    if (blinkLimit <= 0)    // but don't let duty cycle go over 1/7.
+      blinkLimit = 8;
+  }
+} 
+

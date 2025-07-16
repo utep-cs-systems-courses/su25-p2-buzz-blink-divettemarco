7a40d1b656725240b702e8087e142bc02a75a36f
divettemarco <divettemarco@outlook.com>
2025-07-16 03:20:18 -0600

    toggles from red to green LED when switch is held down

---
+++ demos/8-switch-to-toggle-leds/main.c
@@ -0,0 +1,53 @@
+#include <msp430.h>
+#include "libTimer.h"
+
+#define LED_RED BIT0               // P1.0
+#define LED_GREEN BIT6             // P1.6
+#define LEDS (BIT0 | BIT6)
+
+#define SW1 BIT3		/* switch1 is p2.3 */
+#define SWITCHES SW1		/* only 1 switch on this board */
+
+void main(void) 
+{  
+  configureClocks();
+
+  P1DIR |= LEDS;
+  P1OUT &= ~LEDS;		/* leds initially off */
+  
+  P2REN |= SWITCHES;		/* enables resistors for switches */
+  P2IE |= SWITCHES;		/* enable interrupts from switches */
+  P2OUT |= SWITCHES;		/* pull-ups for switches */
+  P2DIR &= ~SWITCHES;		/* set switches' bits for input */
+
+  or_sr(0x18);  // CPU off, GIE on
+} 
+
+void
+switch_interrupt_handler()
+{
+  char p2val = P2IN;		/* switch is in P2 */
+
+/* update switch interrupt sense to detect changes from current buttons */
+  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
+
+/* up=red, down=green */
+  if (p2val & SW1) {
+    P1OUT |= LED_RED;
+    P1OUT &= ~LED_GREEN;
+  } else {
+    P1OUT |= LED_GREEN;
+    P1OUT &= ~LED_RED;
+  }
+}
+
+
+/* Switch on P2 (S2) */
+void
+__interrupt_vec(PORT2_VECTOR) Port_2(){
+  if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
+    P2IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
+    switch_interrupt_handler();	/* single handler for all switches */
+  }
+}
